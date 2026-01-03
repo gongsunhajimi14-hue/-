ebeb699adb5f149d7c9df95aef6dc0c94a4bff20
gongsunhajimi14-hue <gongsunhajimi14@gmail.com>
2026-01-03 18:19:05 +0800

    fix exercise05 constexpr fibonacci

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -7,7 +7,12 @@ constexpr unsigned long long fibonacci(int i) {
         case 1:
             return 1;
         default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+            return [](int n) {
+                unsigned long long a = 0, b = 1;
+                for (int k = 0; k < n; ++k) { auto t = a + b; a = b; b = t; }
+                return a;
+            }(i);
+
     }
 }
 
@@ -19,7 +24,7 @@ int main(int argc, char **argv) {
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
     constexpr auto ANS_N = 90;
-    auto ANS = fibonacci(ANS_N);
+    constexpr auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
