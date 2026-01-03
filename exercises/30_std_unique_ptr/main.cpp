1f64ffa80ddcb27738cb300606416402bba56c37
gongsunhajimi14-hue <gongsunhajimi14@gmail.com>
2026-01-03 19:30:30 +0800

    评测

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -51,11 +51,15 @@ int main(int argc, char **argv) {
 
     std::vector<const char *> answers[]{
         {"fd"},
-        // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"d", "ffd"},
+    #if defined(_WIN32) || defined(_MSC_VER)
+        {"ffr", "d"},
+        {"r", "d", "d"},
+    #else
+        {"d", "ffr"},
         {"d", "d", "r"},
+    #endif
     };
+
 
     // ---- 不要修改以下代码 ----
 
